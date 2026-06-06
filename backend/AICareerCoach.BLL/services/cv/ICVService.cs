8aeb2c3a3b68444775953bf54aab308622928de3
Abdu Yasser <abduyasser24@gmail.com>
2026-06-06 22:47:43 +0300

    Complete CV upload feature with file storage and DB integration 2

---
+++ backend/AICareerCoach.BLL/services/cv/ICVService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AICareerCoach.DAL.Entities;
+
+namespace AICareerCoach.BLL.services.cv
+{
+    public interface ICVService
+    {
+        Task<CV> UploadCVAsync(
+            Stream fileStream,
+            string fileName,
+            int userId);
+
+        List<CV> GetUserCVs(int userId);
+
+        Task DeleteCV(int cvId);
+    }
+}
