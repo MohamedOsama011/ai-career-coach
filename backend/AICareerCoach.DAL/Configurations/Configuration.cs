4f6bf89bcbde5abfdff26dc4ba3cd3adeabc0eb6
shereen-shaaban <sherinshaban67@gmail.com>
2026-06-06 11:44:09 +0300

    feat: seperate database  architecture  in configuration class

--- backend/AICareerCoach.DAL/Configurations/Configuration.cs
+++ backend/AICareerCoach.DAL/Configurations/Configuration.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AICareerCoach.DAL.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
@@ -14,7 +15,23 @@ namespace AICareerCoach.DAL.Configurations
         {
             public void Configure(EntityTypeBuilder<User>builder)
             {
+                //properties
                 builder.HasKey(x => x.Id);
+                builder.Property(x=>x.FullName).HasMaxLength(100).IsRequired();
+                builder.Property(u=>u.Email).IsRequired();
+
+                //relations
+                builder.HasMany(u => u.CVs).WithOne(c => c.User)
+                    .HasForeignKey(c => c.UserId)
+                    .OnDelete(DeleteBehavior.Cascade)
+                    .IsRequired();
+
+                builder.HasMany(u=>u.MockInterviews).WithOne(c => c.User)
+                    .HasForeignKey(i=>i.UserId)
+                    .OnDelete(DeleteBehavior.Cascade)
+                    .IsRequired();
+                    
+
             }
         }
     }
