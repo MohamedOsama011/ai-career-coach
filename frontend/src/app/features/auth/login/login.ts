4dab5ac431236c91740d02fc10363c5ad71ddf01
Mohamed <mohamed.osama.mo493@gmail.com>
2026-06-09 08:08:30 +0300

    Login Component

--- frontend/src/app/features/auth/login/login.ts
+++ frontend/src/app/features/auth/login/login.ts
@@ -1,9 +1,51 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms'; 
+import { AuthService } from '../../../core/services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
-export class Login {}
+export class Login {
+  email: string = '';
+  password: string = '';
+  errorMessage: string = '';
+  isLoading: boolean = false;
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) {}
+
+  onSubmit(){
+    this.errorMessage = '';
+
+    if (!this.email || !this.password) {
+      this.errorMessage = 'Please enter both email and password.';
+      return;
+    }
+
+    this.isLoading = true;
+
+    this.authService.login({
+      email: this.email,
+      password: this.password
+    }).subscribe({
+      next: (response) => {
+        console.log('Login successful:', response);
+        this.authService.saveToken(response.token);
+        this.router.navigate(['/dashboard']);
+        this.isLoading = false;
+      },
+      error: (error) => {
+        console.error('Login failed:', error);
+        this.errorMessage = error.error?.message || 'Invalid email or password. Please try again.';
+        this.isLoading = false;
+      }
+    });
+  }
+}
