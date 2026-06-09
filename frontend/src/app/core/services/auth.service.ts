e731ab542cec6b7dda39a3a828b8cfcaaa391732
Mohamed <mohamed.osama.mo493@gmail.com>
2026-06-09 07:44:04 +0300

    Implement authentication service

---
+++ frontend/src/app/core/services/auth.service.ts
@@ -0,0 +1,30 @@
+import { HttpClient } from "@angular/common/http";
+import { inject, Injectable } from "@angular/core";
+import { AuthResponse, LoginRequest, RegisterRequest } from "../models/user.model";
+import { Observable } from "rxjs";
+
+@Injectable({ providedIn: 'root'})
+
+export class AuthService{
+    private apiUrl = 'http://localhost:5068/api/auth';
+
+    constructor(private http: HttpClient) {}
+
+    register(data: RegisterRequest) : Observable<AuthResponse> {
+        return this.http.post<AuthResponse>(`${this.apiUrl}/register`, data);
+    }
+
+    login(data: LoginRequest) : Observable<AuthResponse> {
+        return this.http.post<AuthResponse>(`${this.apiUrl}/login`, data);
+    }
+    saveToken(token: string): void {
+        localStorage.setItem('authToken', token);
+    }
+    getToken(): string | null {
+        return localStorage.getItem('authToken');
+    }
+    logout(): void {
+        localStorage.removeItem('authToken');
+    }
+}
+
