bd11b8efe5bc05621241c51e585ae78c1e43d384
shereen-shaaban <sherinshaban67@gmail.com>
2026-06-10 23:26:24 +0300

    feat:add forgot &reset pasword ,add new role and assign user to it's role  and add email service to send emails

---
+++ backend/AICareerCoach.BLL/DTOs/Generalresponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AICareerCoach.BLL.DTOs
+{
+    public class Generalresponse
+    {
+        public bool sucess { get; set; }
+        public dynamic Data { get; set; }
+    }
+}
